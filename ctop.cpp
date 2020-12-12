2ae3ddd5469b610ef850e917738810fecc465cc9
deanthebeandip <deanthebean@g.ucla.edu>
2020-12-12 12:55:47 +0800

    first upload

---
+++ ctop.cpp
@@ -0,0 +1,19 @@
+//In this Project, I will translate c++ to python...
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+int main() {
+    // declare ints to store user input
+    // ask for input
+    //cout << "Enter a range of numbers to factorize. Enter lower and upper bounds separated by a space: ";
+    //cin >> lower >> upper ;
+    // vector of strings of size of range to manage a line for every # in range
+    vector<string> output(upper-lower);
+    vector<int> factorCounter(upper-lower);
+
+    cout << "Suck my dick..." << endl;
+
+
+}
