6b925c009c9c472a6fbcc7a0361d5a6da1273b33
deanthebeandip <deanthebean@ucla.edu>
2020-12-11 22:14:02 -0800

    word eater set up...

---
+++ NewData.py
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Suck my dick..." << endl;
+}
--- ctop.cpp
+++ ctop.cpp
@@ -1,19 +1,79 @@
 //In this Project, I will translate c++ to python...
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 
 using namespace std;
+
 int main() {
-    // declare ints to store user input
-    // ask for input
-    //cout << "Enter a range of numbers to factorize. Enter lower and upper bounds separated by a space: ";
-    //cin >> lower >> upper ;
-    // vector of strings of size of range to manage a line for every # in range
-    vector<string> output(upper-lower);
-    vector<int> factorCounter(upper-lower);
 
-    cout << "Suck my dick..." << endl;
+ // Gather name of file from which data will be read from
+    cout << "Enter the name of the file: " ;
+    string file_name = "sample/c1.txt";
+   //getline(cin, file_name);
+    
+    // Create file variable to associate with disk file
+    ifstream input_data;
+    
+    // File is opened
+    input_data.open(file_name);
+    
+        // Check if stream entered fail state
+        if (input_data.fail()) {
+            cout << "Failed to open file." << endl;
+            return 1;
+        }
+    
+        // Define vector that will hold all text on file
+        vector<string> new_data;
+        string temp;
+    
+        // while the text from the user inputted file remains of type string then it will be placed into the new_data vector line by line
+        while (getline(input_data,temp)) {
+            new_data.push_back(temp);
+        }
+    // Close file
+    input_data.close();
+    for(vector<string>::iterator it = new_data.begin(); it != new_data.end();++it){
+		
+	   cout << endl <<*it;
+   }
+   cout <<endl;
+   
+   
+   //Function Space...
+    
+    
+    
+    
+    
+    
+    
+   
+    
+   
+    // Create ofstream object to write to a new file
+    ofstream output_data;
+    
+    // Opens (or creates) NewData file
+    output_data.open("NewData.py");
+    
+        // Check if stream entered fail stat
+        if (output_data.fail()) {
+            cout << "Failed to open file." << endl;
+            return 1;
+        }
+    
+    // for loop that sends in every line of text stored in the vector from the old file into the new file
+	for(vector<string>::iterator it = new_data.begin(); it != new_data.end();++it){
+		output_data << *it << endl;
+	}
+		
+    
+    // Close loop
+    output_data.close();
 
-
+    
+    return 0;
 }
