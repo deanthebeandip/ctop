39085b8bca8ea1f7e1ecf4ec9894ed132358d502
deanthebeandip <deanthebean@ucla.edu>
2020-12-12 00:54:26 -0800

    able to read every character now, even returns...

--- ctop.cpp
+++ ctop.cpp
@@ -7,25 +7,25 @@ int main() {
     cout << "Enter the name of the file: " ;
     string file_name = "sample/c1.txt";
    //getline(cin, file_name);
-   
+
    vector<Word> word_list;
-    
+
     // Create file variable to associate with disk file
     ifstream input_data;
     
     // File is opened
     input_data.open(file_name);
-    
+
         // Check if stream entered fail state
         if (input_data.fail()) {
             cout << "Failed to open file." << endl;
             return 1;
         }
-    
+
         // Define vector that will hold all text on file
         vector<char> new_data;
         string temp;
-    
+
         // while the text from the user inputted file remains of type string then it will be placed into the new_data vector line by line
         if(readfile(file_name, word_list)){
 			cout << "success read" <<endl;
@@ -34,37 +34,37 @@ int main() {
     input_data.close();
 
    for(int i = 0 ; i < (int)word_list.size(); i++){
-	   cout << word_list[i].asc; 
+	   cout << word_list[i].asc;
    }
-   
+
    cout <<endl;
-   
+
    //Function Space...
-   
- 
- 
- 
- 
- 
+
+
+
+
+
+
  /*
 
     // Create ofstream object to write to a new file
     ofstream output_data;
-    
+
     // Opens (or creates) NewData file
     output_data.open("NewData.py");
-    
+
         // Check if stream entered fail stat
         if (output_data.fail()) {
             cout << "Failed to open file." << endl;
             return 1;
         }
-    
+
     // for loop that sends in every line of text stored in the vector from the old file into the new file
 	for(vector<string>::iterator it = new_data.begin(); it != new_data.end();++it){
 		output_data << *it << endl;
 	}
-		
+
     // Close loop
     output_data.close();
 
@@ -72,6 +72,3 @@ int main() {
 */
     return 0;
 }
-
-
-
--- headers.h
+++ headers.h
@@ -5,37 +5,34 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <bits/stdc++.h>
 
 using namespace std;
-
 
 struct Word
 {
     char asc;
 	string asc2;
+	int trueasc; 
     //double duration;
     //int octave;
 };
-
-bool word_comp(vector<Word> &inwords, vector<Word> &outwords, double & tempo)
-{
-
-	return true;
-}
 
 
 bool readfile(string filename, vector<Word> &v )
 {
     bool r = true;
     ifstream ifs(filename);
-    if(ifs)
-    {
+	ifs >> noskipws;
+    if(ifs){
         Word s;
-        while(ifs >> s.asc) 
-            v.push_back(s);
+        while(ifs >> s.asc) {
+				cout << s.asc;
+      			v.push_back(s);
+	        }
     }
-    else {cout<<"Error opening file" <<endl; r = false;}
+    else {
+      cout<<"Error opening file" <<endl; r = false;}
+
 	return r;
 }
 
